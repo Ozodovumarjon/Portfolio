0ce16cbc9eaa408a4d1ed3f244d3abeb9e4f5e63
Umar <ozodovumarjon95@gmail.com>
2024-03-28 02:15:52 +0500

    save

---
+++ assets/Contacts-DwcKST7p.js
@@ -0,0 +1,1 @@
+import{_ as c,c as e,o as t}from"./index-BYTiUR4P.js";const o={},s={class:"h1"};function n(r,_){return t(),e("h1",s,"ВСЕ КОНТАКТЫ НИЖЕ")}const f=c(o,[["render",n]]);export{f as default};
